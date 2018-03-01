782e769140fee744e4cc4eab6ca4159a09a17426
Hamzah <humza.murtaza@gmail.com>
2018-03-01 18:41:27 +0500

    Source-Code

---
+++ src/proxyser/ProxyServer.java
@@ -0,0 +1,38 @@
+package proxyser;
+
+import java.net.*;
+import java.io.*;
+
+public class ProxyServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+
+       
+        int port = 2112;	//default
+
+      try {
+         if (args.length > 0)
+        {
+            port = Integer.parseInt(args[0]);
+            serverSocket = new ServerSocket(port);
+        } 
+         else {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Started on: " + port);
+        }  } catch (IOException e) {
+
+            System.err.println("Could not listen on port: " + args[0]);
+            System.exit(-1);
+        }
+
+        while (listening) {
+            new ProxyThread(serverSocket.accept()).start();
+        }
+        serverSocket.close();
+    }
+}
+
+
+    
+  
